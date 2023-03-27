abea955d979412ad5785b524140972f4a7571378
choc0099 <40311640+choc0099@users.noreply.github.com>
2023-03-28 09:26:38 +1100

    Initial Commit
    Started an empty project for the Bubble Pop Game.

---
+++ Bobble Pop/Bobble Pop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bobble Pop
+//
+//  Created by Christopher Averkos on 28/3/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Bobble Pop/Bobble PopUITests/Bobble_PopUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Bobble_PopUITestsLaunchTests.swift
+//  Bobble PopUITests
+//
+//  Created by Christopher Averkos on 28/3/2023.
+//
+
+import XCTest
+
+final class Bobble_PopUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
