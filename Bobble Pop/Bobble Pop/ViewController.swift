bcf2c814cf0f6a96fa28407e6b7489d1830d5727
choc0099 <40311640+choc0099@users.noreply.github.com>
2023-03-29 18:04:29 +1100

    Connected Buttons and Textfield to the view controller.

--- Bobble Pop/Bobble Pop/ViewController.swift
+++ Bobble Pop/Bobble Pop/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var playerNameText: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func onStartButtonPress(_ sender: Any) {
+        
+    }
+    
+    @IBAction func onSettingsButtonPress(_ sender: Any) {
+        
+    }
+    
 }
 
