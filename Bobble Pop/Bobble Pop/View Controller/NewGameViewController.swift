634a78e8bf8a364a314989760be5a12f4e860adb
choc0099 <40311640+choc0099@users.noreply.github.com>
2023-04-03 16:55:53 +1000

    Linked the buttons to the view
    Achieved some basic navigation.

--- Bobble Pop/Bobble Pop/View Controller/NewGameViewController.swift
+++ Bobble Pop/Bobble Pop/View Controller/NewGameViewController.swift
@@ -1,17 +1,18 @@
 //
-//  SettingsViewController.swift
+//  NewGameViewController.swift
 //  Bobble Pop
 //
 //  Created by Christopher Averkos on 3/4/2023.
 //
 
 import Foundation
+import UIKit
 
-class SettingsViewController: UIViewController {
+class NewGameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    
 }
+
