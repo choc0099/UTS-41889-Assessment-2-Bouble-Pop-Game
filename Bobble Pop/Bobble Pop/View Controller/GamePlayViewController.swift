d408ae8825cf6aa26e1b8342eca7635687fb8c87
choc0099 <40311640+choc0099@users.noreply.github.com>
2023-04-06 06:44:37 +1000

    Implemented timer on the Gam Play View
    Timer has been implemented but does not go to the high score screen just yet.

--- Bobble Pop/Bobble Pop/View Controller/GamePlayViewController.swift
+++ Bobble Pop/Bobble Pop/View Controller/GamePlayViewController.swift
@@ -15,12 +15,32 @@ class GamePlayViewController: UIViewController {
     @IBOutlet weak var currentScoreLabel: UILabel!
     
     var remainingTime = 0
+    var timer = Timer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         remainingTimeLabel.text = String(remainingTime)
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) {
+            timer in
+            self.countingDown()
+        }
     }
+    
+    @objc func countingDown() {
+        remainingTime -= 1
+        remainingTimeLabel.text = String(remainingTime)
+        
+        if remainingTime == 0 {
+            timer.invalidate()
+            //remainingTimeLabel.text = "Done"
+            print("Time is over!")
+            
+        }
+    }
+    
+    
     
     
 }
