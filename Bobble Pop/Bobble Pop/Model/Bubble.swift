30a00db0f50a03f329bace17442dad6cfc3961bf
choc0099 <40311640+choc0099@users.noreply.github.com>
2023-04-10 11:04:56 +1000

    Implemented random colours
    The bubble now appears in random colours based on probability.
    
    This was achieved by using random integers and a switch statement to determine which colour to use based on the number ranges.

--- Bobble Pop/Bobble Pop/Model/Bubble.swift
+++ Bobble Pop/Bobble Pop/Model/Bubble.swift
@@ -14,11 +14,18 @@ class Bubble: UIButton {
     let yPosition = Int.random(in: 170...700)
     //let points: Int?
     
+    var points = 0
+    
     override init(frame: CGRect){
         super.init(frame: frame)
-        self.backgroundColor = .red
+        let randomNumber = Int.random(in: 0...1000)
+        print(randomNumber) // debug
+        self.backgroundColor = selectBubbleColor(randomOnly: randomNumber)
         self.frame = CGRect(x: xPosition, y: yPosition, width: 50, height: 50 )
         self.layer.cornerRadius = 0.5 * self.bounds.size.width
+        
+        //assign the points to the bubble.
+        points = selectPoints(randomOnly: randomNumber)
     }
     
     required init?(coder: NSCoder)
@@ -48,5 +55,50 @@ class Bubble: UIButton {
         flash.repeatCount = 3
         
         layer.add(flash, forKey: nil)
+    }
+    
+    //random colour helper function
+    func selectBubbleColor(randomOnly randomNumber: Int) -> UIColor {
+        //
+        
+        switch randomNumber {
+        case 0...50:
+            print("Black bubble added")
+            return .black
+        case 51...150:
+            print("Blue bubble added")
+            return .blue
+        case 151...300:
+            print("Green button added")
+            return .green
+        case 301...600:
+            print("Pink button added.")
+            return .systemPink
+        default:
+            print("Red button added")
+            return .red
+        }
+    }
+    
+    //helper function to select the points based on button color
+    func selectPoints(randomOnly randomNumber: Int) -> Int
+    {
+        switch randomNumber {
+        case 0...50:
+            return 10
+        case 51...150:
+            return 8
+        case 151...300:
+            return 5
+        case 301...600:
+            return 2
+        default:
+            return 1
+        }
+    }
+    
+    func getPoints() -> Int
+    {
+        return points
     }
 }
