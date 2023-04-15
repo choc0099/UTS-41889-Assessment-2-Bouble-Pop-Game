0134f77319591a049f9750ab45e587cd6ec8bdbf
choc0099 <40311640+choc0099@users.noreply.github.com>
2023-04-15 14:51:12 +1000

    Started modifying the storyboard
    Added a new settings view. Not yet completed.

---
+++ Bobble Pop/Bobble Pop/View Controller/SettingsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsViewController.swift
+//  Bobble Pop
+//
+//  Created by Christopher Averkos on 15/4/2023.
+//
+
+import Foundation
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
