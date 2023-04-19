4f5bc407e2503532404a527ce4c8dfe2f48cd693
choc0099 <40311640+choc0099@users.noreply.github.com>
2023-04-19 15:14:15 +1000

    Transferred readScore method to GameManager class.

--- Bobble Pop/Bobble Pop/Model/HighScoreManager.swift
+++ Bobble Pop/Bobble Pop/Model/HighScoreManager.swift
@@ -8,5 +8,49 @@
 import Foundation
 
 class HighScoreManager {
+  
+    static func readHighScroes() -> [GameScore] {
+        // Read from User Defaults
+        // This should happen at the HighScrollViewController
+        
+        let defaults = UserDefaults.standard;
+        
+        if let savedArrayData = defaults.value(forKey:Game.KEY_HIGH_SCORE) as? Data {
+            if let array = try? PropertyListDecoder().decode(Array<GameScore>.self, from: savedArrayData) {
+                return array
+            } else {
+                return []
+            }
+        } else {
+            return []
+        }
+    }
     
+    func writeHighScore(gameSession game: Game) {
+        // Write high scores to User Defautls
+        let defaults = UserDefaults.standard;
+        // get the current data from current game state.
+        let updatedGameScores = retrieveGameScores(gameSession: game)
+    
+        defaults.set(try? PropertyListEncoder().encode(updatedGameScores), forKey: Game.KEY_HIGH_SCORE)
+    }
+    
+    func retrieveGameScores(gameSession game: Game) -> [GameScore]
+    {
+        var gameScores: [GameScore] = []
+        
+        for player in game.getPlayers()
+        {
+            let currentPlayerName = player.getPlayerName()
+            let currentPlayerHightScore = player.getScore().getHighScore()
+            //convert the class objects to the struct object
+            let currentGameScore: GameScore = GameScore(name: currentPlayerName!, score: currentPlayerHightScore)
+            gameScores.append(currentGameScore)
+            
+        }
+        //print(gameScores) //debug
+        return gameScores
+    }
+    
+
 }
