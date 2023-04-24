35052497bb067fa3c6cbed061df01b691133023f
choc0099 <40311640+choc0099@users.noreply.github.com>
2023-04-24 16:08:00 +1000

    Improved flash annimation
    Enabled reverse for the flash animation used for the countdown.

--- Bobble Pop/Bobble Pop/Model/CustomLabel.swift
+++ Bobble Pop/Bobble Pop/Model/CustomLabel.swift
@@ -28,11 +28,11 @@ class CountDownLabel: UILabel {
     
     func flash() {
         let flash = CABasicAnimation(keyPath: "opacity")
-        flash.duration = 0.7
-        flash.fromValue = 1
-        flash.toValue = 0
+        flash.duration = 0.5
+        flash.fromValue = 0
+        flash.toValue = 1
         flash.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
-        //flash.autoreverses = true
+        flash.autoreverses = true
         flash.repeatCount = 0
         
         layer.add(flash, forKey: nil)
